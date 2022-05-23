f92c3b27644247e37a8ef66d8837c91b1137079f
Meng-Yuan Huang <myh@live.com>
2022-05-23 13:41:25 +0800

    Initial Commit

---
+++ Shared/SwiftTrafficLightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTrafficLightsApp.swift
+//  Shared
+//
+//  Created by 黃孟遠 on 2022/5/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTrafficLightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
